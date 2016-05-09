8bb2cefaf64d2c7c0d2799e8e7a7a31a0ed8c39b
Amio <amio.cn@gmail.com>
2016-05-09 19:17:42 +0800

    Add progress.

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-// const fs = require('fs')
 const program = require('commander')
-const spawn = require('child_process').spawn;
+const spawn = require('child_process').spawn
+const spinner = require('ora')()
+// const parser = require('./lib/parser')
 
 program
   .version(require('./package.json').version)
-  .option('-c, --count', 'Ping every host a specific number of times')
+  .option('-c, --count <n>', 'Ping every host a specific number of times')
   .parse(process.argv)
 
 const opts = program.args.filter(arg => /^-/.test(arg))
 const hosts = program.args.filter(arg => !/^-/.test(arg))
 
+// const total = hosts.length * parseInt(program.count, 10)
+let count = 0
+let progress = ''
+spinner.start()
+
 Promise.all(hosts.map(host => {
   return new Promise((resolve, reject) => {
     let result = ''
     const cp = spawn('ping', [].concat(opts, host))
-    cp.stdout.on('data', data => result = data.toString().trim())
+    cp.stdout.on('data', data => {
+      result = data.toString().trim()
+      count += 1
+      progress += '='
+      spinner.text = `${count} ${progress}`
+    })
     cp.on('close', (code) => {
       if (code === 0) {
         resolve(result)
@@ -24,8 +35,9 @@ Promise.all(hosts.map(host => {
     })
   })
 })).then(results => {
+  spinner.stop()
   results.map(result => {
-    console.log(result, '\n')
+    console.log('\n', result, '\n')
   })
 }).catch(reason => {
   console.error(reason)
