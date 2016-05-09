b423c66b8be78a1ef2e8da6b4ebab37b20c08a35
Amio <amio.cn@gmail.com>
2016-05-09 19:45:17 +0800

    Replace commander with meow.

--- index.js
+++ index.js
@@ -1,44 +1,19 @@
-const program = require('commander')
-const spawn = require('child_process').spawn
-const spinner = require('ora')()
-// const parser = require('./lib/parser')
+const meow = require('meow')
+const main = require('./lib/main')
 
-program
-  .version(require('./package.json').version)
-  .option('-c, --count <n>', 'Ping every host a specific number of times')
-  .parse(process.argv)
+const cli = meow(`
+	Usage
+	  $ foo <input>
 
-const opts = program.args.filter(arg => /^-/.test(arg))
-const hosts = program.args.filter(arg => !/^-/.test(arg))
+	Options
+	  -c, --count  Ping every host a specific number of times
 
-// const total = hosts.length * parseInt(program.count, 10)
-let count = 0
-let progress = ''
-spinner.start()
-
-Promise.all(hosts.map(host => {
-  return new Promise((resolve, reject) => {
-    let result = ''
-    const cp = spawn('ping', [].concat(opts, host))
-    cp.stdout.on('data', data => {
-      result = data.toString().trim()
-      count += 1
-      progress += '='
-      spinner.text = `${count} ${progress}`
-    })
-    cp.on('close', (code) => {
-      if (code === 0) {
-        resolve(result)
-      } else {
-        resolve({code: code, msg: `Ping ${host} exited with code ${code}`})
-      }
-    })
-  })
-})).then(results => {
-  spinner.stop()
-  results.map(result => {
-    console.log('\n', result, '\n')
-  })
-}).catch(reason => {
-  console.error(reason)
+	Examples
+	  $ foo unicorns --rainbow
+`, {
+	alias: {
+		c: 'count'
+	}
 })
+
+main(cli.input, cli.flags)
