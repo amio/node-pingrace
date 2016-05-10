df76644715e594647f661937b8cffd31ecc9131b
Amio <amio.cn@gmail.com>
2016-05-10 11:34:13 +0800

    Add babel require hook for nodejs < 6.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const meow = require('meow')
-const main = require('./lib/main')
+var NODE_MAJOR = parseInt(process.version.match(/^v(\d+)\./)[1])
+require('babel-register')({
+	presets: [ NODE_MAJOR === 6 ? 'node6' : 'es2015' ]
+})
 
-const cli = meow(`
+var meow = require('meow')
+var main = require('./lib/main')
+
+var cli = meow(`
 	Usage
 	  $ foo <input>
 
