783f0ce7ec4222d2a5b6d5e338affc038a8752f3
Amio <amio.cn@gmail.com>
2016-05-10 17:13:46 +0800

    Rename index.js to cli.js.

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import commonjs from 'rollup-plugin-commonjs'
 import nodeResolve from 'rollup-plugin-node-resolve'
 
 export default {
-  entry: 'index.js',
+  entry: 'cli.js',
   dest: 'dist/index.js',
   format: 'iife',
   plugins: [
