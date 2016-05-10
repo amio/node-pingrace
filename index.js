15f68ca16646af94dae5052328c672bd5e8f4feb
Amio <amio.cn@gmail.com>
2016-05-10 15:00:23 +0800

    Add rollup build.

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-var NODE_MAJOR = parseInt(process.version.match(/^v(\d+)\./)[1])
-require('babel-register')({
-  presets: [NODE_MAJOR === 6 ? 'node6' : 'es2015']
-})
-
 var meow = require('meow')
-var main = require('./lib/main')
+import main from './lib/main'
 
 var minimistOptions = {
   alias: { c: 'count' }
---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import nodeResolve from 'rollup-plugin-node-resolve'
+
+export default {
+  entry: 'index.js',
+  dest: 'dist/index.js',
+  format: 'iife',
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+      presets: ['es2015-rollup']
+    }),
+    nodeResolve({
+      jsnext: true,
+      main: true
+    }),
+    commonjs({
+      include: 'node_modules/**'
+    })
+  ]
+}
