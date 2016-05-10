76d4d081c35ae8fa6cc118be38c1215ef9ef8aca
Amio <amio.cn@gmail.com>
2016-05-10 12:11:33 +0800

    Add README.md.

---
+++ README.md
@@ -0,0 +1,14 @@
+# pingrace
+Ping hosts to see which one is faster: a ping race program.
+
+```
+Usage
+  $ pingrace <host> [<host>...]
+
+Options
+  -c, --count  Ping every host a specific number of times (default: 10)
+
+Examples
+  $ pingrace a.example.com b.example.com c.example.com
+  $ pingrace -c 10 a.example.com b.example.com c.example.com
+```
--- index.js
+++ index.js
@@ -11,14 +11,15 @@ var minimistOptions = {
 }
 
 var cli = meow(`
-	Usage
-	  $ foo <input>
+  Usage
+    $ pingrace <host> [<host>...]
 
-	Options
-	  -c, --count  Ping every host a specific number of times
+  Options
+    -c, --count  Ping every host a specific number of times (default: 10)
 
-	Examples
-	  $ foo unicorns --rainbow
+  Examples
+    $ pingrace a.example.com b.example.com c.example.com
+    $ pingrace -c 10 a.example.com b.example.com c.example.com
 `, minimistOptions)
 
 main(cli.input, cli.flags)
