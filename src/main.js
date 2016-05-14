fc9097739700e71ba9b06297bac6b881d143e156
Amio <amio.cn@gmail.com>
2016-05-15 00:39:00 +0800

    Create printer.js.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 const spinner = require('ora')()
 const spawn = require('child_process').spawn
-import { parseLog, parseStatics } from './parser'
-import padStart from 'lodash/padStart'
-import padEnd from 'lodash/padEnd'
+import { parseLog } from './parser'
+import { printStaticsTable } from './printer'
 
 export default function (hosts, flags) {
   const count = flags.count ? parseInt(flags.count) : 10
@@ -40,41 +39,4 @@ export default function (hosts, flags) {
     process.stdout.write(reason)
     process.exit(1)
   })
-}
-
-function printStaticsTable (results) {
-  const title = `. . . . . . . RACING RESULT (${results.length}) . . . . . . .`
-  process.stdout.write(`\n                ${title}\n\n`)
-
-  const headers = {
-    host: '',
-    lossRate: 'loss',
-    avg: 'avg(ms)',
-    deviation: 'deviation',
-    stddev: 'stddev(ms)'
-  }
-  printStaticsTableRow(headers)
-  results
-    .map(result => parseStatics(result))
-    .sort((a, b) => {
-      const [al, bl] = [parseFloat(a.lossRate), parseFloat(b.lossRate)]
-      return al === bl ? (a.avg - b.avg) : (al - bl)
-    })
-    .forEach(result => printStaticsTableRow(result))
-
-  process.stdout.write('\n')
-}
-
-function printStaticsTableRow (statics) {
-  const {avg, min, max} = statics
-  if (!statics.deviation) {
-    statics.deviation = ('± ' + Math.abs(avg * 2 - min - max) / avg).slice(0, 6) + '%'
-  }
-  process.stdout.write([
-    padEnd(statics.host, 20, ' '),
-    padStart(statics.lossRate, 12, ' '),
-    padStart(Math.round(avg) || avg, 12, ' '),
-    padStart(statics.deviation, 12, ' '),
-    padStart(statics.stddev, 12, ' ')
-  ].join('') + '\n')
 }
---
+++ src/printer.js
@@ -0,0 +1,40 @@
+import { parseStatics } from './parser'
+import padStart from 'lodash/padStart'
+import padEnd from 'lodash/padEnd'
+
+export function printStaticsTable (results) {
+  const title = `. . . . . . . RACING RESULT (${results.length}) . . . . . . .`
+  process.stdout.write(`\n           ${title}\n\n`)
+
+  const headers = {
+    host: '',
+    lossRate: 'loss',
+    avg: 'avg(ms)',
+    deviation: 'deviation',
+    stddev: 'stddev(ms)'
+  }
+  printStaticsTableRow(headers)
+  results
+    .map(result => parseStatics(result))
+    .sort((a, b) => {
+      const [al, bl] = [parseFloat(a.lossRate), parseFloat(b.lossRate)]
+      return al === bl ? (a.avg - b.avg) : (al - bl)
+    })
+    .forEach(result => printStaticsTableRow(result))
+
+  process.stdout.write('\n')
+}
+
+function printStaticsTableRow (statics) {
+  const {avg, min, max} = statics
+  if (!statics.deviation) {
+    statics.deviation = ('± ' + Math.abs(avg * 2 - min - max) / avg).slice(0, 6) + '%'
+  }
+  process.stdout.write([
+    padEnd(statics.host, 20, ' '),
+    padStart(statics.lossRate, 12, ' '),
+    padStart(Math.round(avg) || avg, 12, ' '),
+    padStart(statics.deviation, 12, ' '),
+    padStart(statics.stddev, 12, ' ')
+  ].join('') + '\n')
+}
