fb6bc6ff3659584a8a3a18e095559a450f8afe07
Amio <amio.cn@gmail.com>
2016-08-11 14:30:25 +0800

    Refine output format for long host.

--- src/printer.js
+++ src/printer.js
@@ -3,37 +3,48 @@ const padStart = require('lodash.padstart')
 const padEnd = require('lodash.padend')
 
 export function printStaticsTable (results) {
-  const title = `. . . . . . RACING RESULT (${results.length}) . . . . . .`
-  process.stdout.write(`\n             ${title}\n\n`)
+  // parse raw results and sort
+  results = results.map(result => parseStatics(result)).sort((a, b) => {
+    const [al, bl] = [parseFloat(a.lossRate), parseFloat(b.lossRate)]
+    return al === bl ? (a.avg - b.avg) : (al - bl)
+  })
 
-  const headers = {
+  const hostMaxLength = results.reduce(function (prev, curr) {
+    return curr.host.length > prev ? curr.host.length : prev
+  }, 0)
+
+  // print title
+  let title = padStart(
+    `. . . . . RACING RESULT (${results.length}) . . . . .`,
+    Math.floor(hostMaxLength / 2) + 42,
+    ' '
+  )
+  process.stdout.write(`\n${title}\n\n`)
+
+  // print headers
+  printStaticsTableRow({
     host: '',
     lossRate: 'loss',
     avg: 'avg(ms)',
     deviation: 'deviation',
     stddev: 'stddev(ms)'
-  }
-  printStaticsTableRow(headers)
-  results
-    .map(result => parseStatics(result))
-    .sort((a, b) => {
-      const [al, bl] = [parseFloat(a.lossRate), parseFloat(b.lossRate)]
-      return al === bl ? (a.avg - b.avg) : (al - bl)
-    })
-    .forEach(result => printStaticsTableRow(result))
+  }, hostMaxLength)
+
+  // print table rows
+  results.forEach(result => printStaticsTableRow(result, hostMaxLength))
 
   process.stdout.write('\n')
 }
 
-function printStaticsTableRow (statics) {
+function printStaticsTableRow (statics, hostMaxLength) {
   const {avg, min, max} = statics
   if (!statics.deviation) {
     statics.deviation = ('± ' + Math.abs(avg * 2 - min - max) / avg).slice(0, 6) + '%'
   }
   process.stdout.write([
-    padEnd(statics.host, 20, ' '),
-    padStart(statics.lossRate, 12, ' '),
-    padStart(Math.round(avg) || avg, 12, ' '),
+    padEnd(statics.host, hostMaxLength, ' '),
+    padStart(statics.lossRate, 10, ' '),
+    padStart(Math.round(avg) || avg, 10, ' '),
     padStart(statics.deviation, 12, ' '),
     padStart(statics.stddev, 12, ' ')
   ].join('') + '\n')
